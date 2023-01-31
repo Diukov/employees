d24e97e559b9665aa0cb684549803094f09fa839
Dmitriy Diukov <60452482+Diukov@users.noreply.github.com>
2023-01-31 17:33:55 +0200

    state for components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,18 +3,55 @@ import SearchBar from '../app-search-bar/app-search-bar';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../app-employees-list/app-employees-list';
 import EmployeesAddForm from '../app-employees-add-form/app-employees-add-form';
+import { Component } from 'react';
 
 import './app.css';
 
-function WhoAmI(props) {
-  return (
-    <div>
-      <h1>
-        My name is {props.name}, surname - {props.surname}
-      </h1>
-      <a href="{props.link}">My profile</a>
-    </div>
-  );
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: 28,
+      position: ''
+    }
+  }
+
+  nextYear = () => {
+    this.setState(state => ({
+      age: state.age + 1
+    }))
+  }
+
+  commitInputChanges = (event, color) => {
+    console.log(color);
+
+    this.setState({
+      position: event.target.value
+    })
+  }
+
+  render() {
+    const { name, surname, link } = this.props;
+    const { position, age } = this.state;
+
+    return (
+      <div>
+        <button onClick={this.nextYear}>
+          +++
+        </button>
+        <h1>
+          My name is {name}, surname - {surname}, age - {age}, position - {position}
+        </h1>
+        <a href={link}>My profile</a>
+        <form>
+          <span>
+            Enter your posiion
+          </span>
+          <input type="text" onChange={(event) => this.commitInputChanges(event, 'some color')} />
+        </form>
+      </div >
+    );
+  }
 };
 
 function App() {
@@ -38,6 +75,7 @@ function App() {
       <EmployeesAddForm />
 
       <WhoAmI name="John" surname="Smith" link="facebook.com" />
+      <WhoAmI name="Alex" surname="Lolongi" link="facebook.com" />
     </div>
   );
 }
