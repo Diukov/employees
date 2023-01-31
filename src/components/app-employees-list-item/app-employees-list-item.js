9bca95eaa5a74e75151888a042e571f4ee89d0d7
Dmitriy Diukov <60452482+Diukov@users.noreply.github.com>
2023-01-31 20:01:22 +0200

    state for components

--- src/components/app-employees-list-item/app-employees-list-item.js
+++ src/components/app-employees-list-item/app-employees-list-item.js
@@ -1,27 +1,54 @@
 import './app-employees-list-item.css';
+import { Component } from 'react';
 
-const EmployeesListItem = ({ name, salary, increase }) => {
-  const increaseClass = increase ? "increase" : "";
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      increase: false,
+      promotion: false
+    }
+  }
 
-  return (
-    <li className={'list-group-item d-flex justify-content-between ' + increaseClass}>
-      <span className="list-group-item-label">
-        {name}
-      </span>
-      <input type="text" className="list-group-item-input" defaultValue={'$' + salary} />
-      <div className="d-flex justify-content-center align-items-center">
-        <button type="button"
-          className="btn-cookie btn-sm">
-          <i className="fas fa-cookie"></i>
-        </button>
-        <button type="button"
-          className="btn-trash btn-sm">
-          <i className="fas fa-trash"></i>
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
+  onIncrease = () => {
+    this.setState(({ increase }) => ({
+      increase: !increase
+    }))
+  }
+
+  toPromotion = () => {
+    this.setState(({ promotion }) => ({
+      promotion: !promotion
+    }))
+  }
+
+  render() {
+    const { name, salary } = this.props;
+    const { increase, promotion } = this.state;
+    const increaseClass = increase ? " increase" : "";
+    const promotionClass = promotion ? " like" : "";
+
+    return (
+      <li className={"list-group-item d-flex justify-content-between" + increaseClass + promotionClass} >
+        <span className={"list-group-item-label"} onClick={this.toPromotion}>
+          {name}
+        </span>
+        <input type="text" className="list-group-item-input" defaultValue={'$' + salary} />
+        <div className="d-flex justify-content-center align-items-center">
+          <button type="button"
+            className="btn-cookie btn-sm"
+            onClick={this.onIncrease}>
+            <i className="fas fa-cookie"></i>
+          </button>
+          <button type="button"
+            className="btn-trash btn-sm">
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
 };
 
 export default EmployeesListItem;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,56 +3,8 @@ import SearchBar from '../app-search-bar/app-search-bar';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../app-employees-list/app-employees-list';
 import EmployeesAddForm from '../app-employees-add-form/app-employees-add-form';
-import { Component } from 'react';
 
 import './app.css';
-
-class WhoAmI extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      age: 28,
-      position: ''
-    }
-  }
-
-  nextYear = () => {
-    this.setState(state => ({
-      age: state.age + 1
-    }))
-  }
-
-  commitInputChanges = (event, color) => {
-    console.log(color);
-
-    this.setState({
-      position: event.target.value
-    })
-  }
-
-  render() {
-    const { name, surname, link } = this.props;
-    const { position, age } = this.state;
-
-    return (
-      <div>
-        <button onClick={this.nextYear}>
-          +++
-        </button>
-        <h1>
-          My name is {name}, surname - {surname}, age - {age}, position - {position}
-        </h1>
-        <a href={link}>My profile</a>
-        <form>
-          <span>
-            Enter your posiion
-          </span>
-          <input type="text" onChange={(event) => this.commitInputChanges(event, 'some color')} />
-        </form>
-      </div >
-    );
-  }
-};
 
 function App() {
   const data = [
@@ -73,9 +25,6 @@ function App() {
       <EmployeesList data={data} />
 
       <EmployeesAddForm />
-
-      <WhoAmI name="John" surname="Smith" link="facebook.com" />
-      <WhoAmI name="Alex" surname="Lolongi" link="facebook.com" />
     </div>
   );
 }
