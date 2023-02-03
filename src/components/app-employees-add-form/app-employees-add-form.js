f39af9e35148544f1911215941bb997d57309e76
Dmitriy Diukov <60452482+Diukov@users.noreply.github.com>
2023-02-03 17:28:39 +0200

    controlled inputs added

--- src/components/app-employees-add-form/app-employees-add-form.js
+++ src/components/app-employees-add-form/app-employees-add-form.js
@@ -1,20 +1,49 @@
 import './app-employees-add-form.css';
+import { Component } from 'react';
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>
-        Add new employee
-      </h3>
-      <form action="POST" className="add-form d-flex">
-        <input type="text" className="form-control new-post-label" placeholder="What's his name?" />
-        <input type="number" className="form-control new-post-label" placeholder="What's his salary?" />
-        <button type="submit" className="btn btn-outline-light">
-          Add
-        </button>
-      </form>
-    </div>
-  );
-};
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      salary: ''
+    }
+  }
+
+  onValueChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    const { name, salary } = this.state;
+
+    return (
+      <div className="app-add-form">
+        <h3>
+          Add new employee
+        </h3>
+        <form action="POST" className="add-form d-flex">
+          <input type="text"
+            className="form-control new-post-label"
+            placeholder="What's his name?"
+            name="name"
+            value={name}
+            onChange={this.onValueChange} />
+          <input type="number"
+            className="form-control new-post-label"
+            placeholder="What's his salary?"
+            name="salary"
+            value={salary}
+            onChange={this.onValueChange} />
+          <button type="submit" className="btn btn-outline-light">
+            Add
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default EmployeesAddForm;
